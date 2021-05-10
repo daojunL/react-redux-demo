c327d667bfff0000b8070c59c8d86959619c8686
daojun <daojunliang@outlook.com>
2021-05-10 15:05:10 +0800

    Add another section - buy ice creams

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { Provider } from 'react-redux'
 import store from './redux/store'
 import './App.css';
 import CakeContainer from './components/CakeContainer'
+import IceCreamContainer from './components/IceCreamContainer'
+import HooksCakeContainer from './components/HooksCakeContainer'
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HooksCakeContainer />
         <CakeContainer />
+        <IceCreamContainer />
       </div>
     </Provider>
   );
