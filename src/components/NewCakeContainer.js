f2bf610fe8eda6e358e25b4eb0d1c67736e13912
daojun <daojunliang@outlook.com>
2021-05-10 16:21:42 +0800

    bug fix

--- src/components/NewCakeContainer.js
+++ src/components/NewCakeContainer.js
@@ -7,7 +7,7 @@ function NewCakeContainer(props) {
     return ( 
         <div>
             <h2> Number of Cakes - {props.numOfCakes} </h2>
-            <input type="text" value={number} onChange={e => SVGAnimatedNumber(e.target.value)} />
+            <input type="text" value={number} onChange={e => setNumber(e.target.value)} />
             <button onClick={() => props.buyCake(number)}>Buy {number} Cake</button>
         </div>
     );
