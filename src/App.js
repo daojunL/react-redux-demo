cf8219ce51d7bf2b5cd9d9527760af40929895ba
daojun <daojunliang@outlook.com>
2021-05-10 16:09:57 +0800

    create a new component newCakeComponent and pass payload as an argument

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import CakeContainer from './components/CakeContainer'
 import IceCreamContainer from './components/IceCreamContainer'
 import HooksCakeContainer from './components/HooksCakeContainer'
+import NewCakeContainer from './components/NewCakeContainer'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer />
+        <NewCakeContainer />
       </div>
     </Provider>
   );
